92ca7d948c0ce5e7c19a7b728a4fca03b833e50d
LynnDarinKim <darin.kim.1128@gmail.com>
2020-12-11 15:43:48 -0400

    modified component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
@@ -18,11 +18,11 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent,
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
+    ShoppingEditComponent,
 
   ],
   imports: [
